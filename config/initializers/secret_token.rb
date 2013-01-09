179b8b254903ca81c1627a84b4ad2bea924edbab
artdvl <artdvl91@gmail.com>
2013-01-09 13:22:09 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simta::Application.config.secret_token = 'ffdf5614877eaf905c1411d75391f80f4f03059cb498935a74f58dd754578b5c678ed0c6f1a23d9c12e9602ff3e41c7808c53051aa39d1f449a78ed7a8ae45f1'
